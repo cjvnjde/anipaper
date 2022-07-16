7b8e48bec85157a4b92bf42677a7e3a2d76cc0be
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2022-07-16 15:46:36 +0300

    update pwa

--- next.config.js
+++ next.config.js
@@ -4,14 +4,14 @@ const runtimeCaching = require("next-pwa/cache");
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   env: {
     apiKey: '0a428092f0e3c504cae8f24b4177e94b',
   },
   pwa: {
     dest: "public",
     runtimeCaching,
-    disable: process.env.NODE_ENV === "development",
+    register: true,
+    skipWaiting: true,
     fallbacks: {
       document: "/pages/offline.tsx",
     },
